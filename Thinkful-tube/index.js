0f00b48b40f1920fa0a582e31c7ef8a861cc3319
hckia <hckia1@gmail.com>
2017-12-12 01:39:06 -0800

    Create index.js

---
+++ Thinkful-tube/index.js
@@ -0,0 +1,58 @@
+const TUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  const settings = {
+    url: TUBE_SEARCH_URL,
+    data: {
+      part: 'snippet',
+      q: searchTerm,
+      key: 'AIzaSyC2_O5A8h-8RZsdhIZXjdeyp3TZw13fyVo',
+      maxResults: 5
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings).fail(function (jqXHR, text) { 
+    console.log(JSON.stringify(jqXHR));
+    console.log(text);
+  });
+}
+
+function renderResult(result) {
+  // console.log(result.id.videoId);
+  var resultChannelID = result.id.channelId;
+  var resultVidID = result.id.videoId;
+  console.log(resultChannelID);
+  console.log(resultVidID);
+  var resultID;
+  if(resultVidID === undefined){resultID = 'https://www.youtube.com/channel/' + resultChannelID} else {resultID = 'https://www.youtube.com/watch?v=' + resultVidID} 
+  console.log(resultID);
+  return `
+    <div>
+    // when you don't know WTF the response will be.
+    <h2>${result.snippet.channelTitle}</h2>
+    <p class="result-description">${result.snippet.description}</p>
+    <a href="${resultID}" target= _blank><img src="${result.snippet.thumbnails.default.url}"></a>
+      ${JSON.stringify(result)};
+    </div>
+  `;
+}
+
+function displayTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayTubeSearchData);
+  });
+}
+
+$(watchSubmit);

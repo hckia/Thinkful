6a7a3cac6376c78f168c2fe979cf1956cefcfa50
hckia <hckia1@gmail.com>
2018-01-08 16:13:32 -0800

    Create compare-dates.js

---
+++ notes/algorithims/compare-dates.js
@@ -0,0 +1,7 @@
+let startDate = Date.parse("2010-07-17");
+let endDate = Date.parse("2018-01-07");
+
+console.log('startDate:', startDate);
+console.log('endDate:', endDate);
+
+console.log('startDate < endDate ==', startDate < endDate);

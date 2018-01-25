31a76a9dc8db92ba938a74dcf794733c350dfba4
hckia <hckia1@gmail.com>
2018-01-25 13:01:21 -0800

    Create typeit.js

---
+++ notes/algorithims/typeit.js
@@ -0,0 +1,27 @@
+#TypeIt - typecasting on a site with Jquery.
+
+[Jquery TypeIt](https://www.hongkiat.com/blog/code-typing-text-animations-jquery-typeit/)
+```
+$('.type-it').typeIt({
+	 lifeLike: true,
+     autoStart: false,
+	 loop: true,
+	 loopDelay: 5000
+})
+.tiType('Type some stufff here')
+.tiPause(2000)
+.tiDelete()
+.tiType('Type some stufff here again')
+.tiPause(2000)
+.tiDelete()
+.tiType('Type some other stufff here')
+.tiPause(2000)
+.tiDelete()
+.tiType('etc')
+.tiPause(2000)
+.tiDelete()
+.tiType('etc')
+.tiPause(2000)
+.tiDelete()
+.tiType('etc!');
+```
